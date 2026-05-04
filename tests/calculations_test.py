7c49b43561af74d1fec49ac45e6c49f40cf832fe
Ayeisha23 <eishatucker23@gmail.com>
2026-05-04 07:44:09 +0000

    Reenable unit test

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,12 +59,11 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
- """Test with n=10."""
- # Arrange
- n = 10
+    """Test with n=10."""
+    # Arrange
+    n = 10
 
- # Act
- result = get_nth_fibonacci(n)
-
- # Assert
- assert result == 89
+    # Act
+    result = get_nth_fibonacci(n)
+    
+assert result == 89
